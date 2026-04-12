e844039e45bf8d4f9a679ba5e2e0e158de0a4368
abuahammedfaisal <abuahammedfaisal@gmail.com>
2026-04-12 21:28:10 +0600

    Initial project upload

---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.netCoreCrudDemo.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display (Name ="Student ID")]
+        public int StudentId { get; set; }
+        [Required]
+        [Display(Name ="Student Name")]
+        public string StudentName { get; set; }
+        public string Email {  get; set; }
+        public string Course {  get; set; }
+       
+        [Display(Name = "Enorllment Date")]
+        public DateTime EnorllmentDate {  get; set; }
+
+    }
+}
